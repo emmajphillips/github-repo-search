6ac0980630897fed56c83e621200192f5f838429
emmajphillips <emmjphillips@gmail.com>
2020-07-16 21:54:31 +0100

    added event handlers to form to fetch data from GitHub API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
         <v-card color="green">
           <h3 class="text-center">Hello</h3>
           <v-card-text>
-            <v-form v-model="valid">
+            <v-form v-model="valid" v-on:submit.prevent="fetchResults">
               <v-text-field
                 v-model="query"
                 label="Search GitHub"
@@ -14,7 +14,9 @@
             </v-form>
           </v-card-text>
           <v-card-actions>
-            <v-btn right color="warning" round>Search</v-btn>
+            <v-btn right color="warning" rounded v-on:click="fetchResults"
+              >Search</v-btn
+            >
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -24,13 +26,28 @@
 
 <script lang="ts">
 import Vue from "vue";
+import axios from "axios";
 
 export default Vue.extend({
   name: "HelloWorld",
 
   data: () => ({
     valid: false,
-    query: ""
-  })
+    query: "",
+    repositories: []
+  }),
+  methods: {
+    async fetchResults() {
+      try {
+        const response = await axios.get(
+          "https://api.github.com/users/airbnb/repos?type=owner&per_page=25&page=1",
+          { headers: { Accept: "application/vnd.github.v3+json" } }
+        );
+        console.log(response.data);
+      } catch (err) {
+        console.log("error", err);
+      }
+    }
+  }
 });
 </script>
