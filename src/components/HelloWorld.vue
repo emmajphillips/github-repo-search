0ffe57a03b67781c000ad6d2762fda6f0b66d98d
emmajphillips <emmjphillips@gmail.com>
2020-07-17 09:51:00 +0100

    connected query to results display

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,53 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex xs12 sm8 offset-sm2 lg6 offset-lg3>
-        <v-card color="green">
-          <h3 class="text-center">Hello</h3>
-          <v-card-text>
-            <v-form v-model="valid" v-on:submit.prevent="fetchResults">
-              <v-text-field
-                v-model="query"
-                label="Search GitHub"
-                color="black"
-              />
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn right color="warning" rounded v-on:click="fetchResults"
-              >Search</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <v-container>
+      <v-layout>
+        <v-flex xs12 sm8 offset-sm2 lg6 offset-lg3>
+          <h1 class="display-3 text-center">Hello</h1>
+          <v-form v-model="valid" v-on:submit.prevent="fetchResults">
+            <v-text-field v-model="query" label="Search GitHub" color="black" />
+          </v-form>
+          <v-btn block color="warning" rounded v-on:click="fetchResults"
+            >Search</v-btn
+          >
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container grid-list-md>
+      <v-layout row wrap justify-center>
+        <v-flex
+          v-for="repository in repositories"
+          :key="`${repository.id}`"
+          xs12
+          sm8
+          lg6
+        >
+          <v-card>
+            <v-container fluid>
+              <v-layout row>
+                <v-flex xs 5>
+                  <v-avatar tile>
+                    <img
+                      :src="`${repository.owner.avatar_url}`"
+                      :alt="`${repository.name}`"
+                    />
+                  </v-avatar>
+                </v-flex>
+                <v-flex xs 7>
+                  <v-card-text>
+                    <a :href="`${repository.html_url}`" target="_blank">
+                      <h3>{{ repository.name }}</h3>
+                    </a>
+                    <p>{{ repository.created_at }}</p>
+                  </v-card-text>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+        <v-pagination v-model="page"></v-pagination>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 
@@ -34,18 +61,20 @@ export default Vue.extend({
   data: () => ({
     valid: false,
     query: "",
+    page: 1,
     repositories: []
   }),
   methods: {
     async fetchResults() {
       try {
+        if (!this.query) return;
         const response = await axios.get(
-          "https://api.github.com/users/airbnb/repos?type=owner&per_page=25&page=1",
+          `https://api.github.com/users/${this.query}/repos?type=owner&per_page=25&page=${this.page}`,
           { headers: { Accept: "application/vnd.github.v3+json" } }
         );
-        console.log(response.data);
+        this.repositories = response.data;
       } catch (err) {
-        console.log("error", err);
+        console.log("Error:", err);
       }
     }
   }
