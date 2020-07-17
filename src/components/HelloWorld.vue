82c41d341fe5c10957765ff17226452ae403c753
emmajphillips <emmjphillips@gmail.com>
2020-07-17 13:38:42 +0100

    added fetchBranches axios get request, connected to event handler on 'see branches' button, currently console logging branches

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,10 +4,10 @@
       <v-layout>
         <v-flex xs12 sm8 offset-sm2 lg6 offset-lg3>
           <h1 class="display-3 text-center">Hello</h1>
-          <v-form v-model="valid" v-on:submit.prevent="fetchResults">
+          <v-form v-model="valid" v-on:submit.prevent="fetchRepositories">
             <v-text-field v-model="query" label="Search GitHub" color="black" />
           </v-form>
-          <v-btn block color="warning" rounded v-on:click="fetchResults"
+          <v-btn block color="warning" rounded v-on:click="fetchRepositories"
             >Search</v-btn
           >
         </v-flex>
@@ -22,31 +22,38 @@
           sm8
           lg6
         >
+          <!-- Make card into own component -->
           <v-card>
             <v-container fluid>
               <v-layout row>
-                <v-flex xs 5>
-                  <v-avatar tile>
+                <v-flex xs3>
+                  <v-avatar tile size="100">
                     <img
                       :src="`${repository.owner.avatar_url}`"
                       :alt="`${repository.name}`"
                     />
                   </v-avatar>
                 </v-flex>
-                <v-flex xs 7>
+                <v-flex xs9>
                   <v-card-text>
                     <a :href="`${repository.html_url}`" target="_blank">
                       <h3>{{ repository.name }}</h3>
                     </a>
                     <p>{{ repository.created_at }}</p>
+                    <v-btn
+                      small
+                      color="primary"
+                      v-on:click="fetchBranches(repository.name)"
+                      >See branches</v-btn
+                    >
                   </v-card-text>
                 </v-flex>
               </v-layout>
             </v-container>
           </v-card>
         </v-flex>
-        <v-pagination v-model="page"></v-pagination>
       </v-layout>
+      <v-pagination v-model="page"></v-pagination>
     </v-container>
   </v-container>
 </template>
@@ -60,19 +67,33 @@ export default Vue.extend({
 
   data: () => ({
     valid: false,
+    baseUrl: "https://api.github.com",
     query: "",
     page: 1,
-    repositories: []
+    repositories: [],
+    branches: []
   }),
   methods: {
-    async fetchResults() {
+    async fetchRepositories() {
       try {
         if (!this.query) return;
         const response = await axios.get(
-          `https://api.github.com/users/${this.query}/repos?type=owner&per_page=25&page=${this.page}`,
+          `${this.baseUrl}/users/${this.query}/repos?type=owner&per_page=25&page=${this.page}`,
           { headers: { Accept: "application/vnd.github.v3+json" } }
         );
         this.repositories = response.data;
+      } catch (err) {
+        console.log("Error:", err);
+      }
+    },
+    async fetchBranches(name: string) {
+      try {
+        const response = await axios.get(
+          `${this.baseUrl}/repos/${this.query}/${name}/branches`,
+          { headers: { Accept: "application/vnd.github.v3+json" } }
+        );
+        this.branches = response.data;
+        console.log(this.branches);
       } catch (err) {
         console.log("Error:", err);
       }
